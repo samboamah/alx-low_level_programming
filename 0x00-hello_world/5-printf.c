d179860084741314b36ce0fb7dad6d24f0d0f763
Samuel Boamah <skboamah16@gmail.com>
2022-09-09 00:08:26 +0000

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print the string in the printf function
+ *
+ * Description: using the main function
+ * This program prints "with proper grammer, but the outcome is a piece of art"
+ * Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
