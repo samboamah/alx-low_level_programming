9cd7da536b60fff3208b53a13bbcda785bb2e45d
Samuel Boamah <skboamah16@gmail.com>
2022-09-09 01:25:53 +0000

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - print the string put in the function
- *
- * Description: using the main function.
- * prints exactly "and that piece of art is useful - Dora Korpar, 2015-19-19"
- * followed by a new line to the standard error.
- *Return: 1
- */
+* main - print quote
+*
+* Description: prints Dora's quote
+*
+* Return: Always 1 (Success)
+*/
+
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-        return (1);
+	return (1);
 }
