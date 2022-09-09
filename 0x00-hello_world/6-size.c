c35a99608d275ef45c54c5767e0a1e708c791eea
Samuel Boamah <skboamah16@gmail.com>
2022-09-09 00:50:07 +0000

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
  * Description - using the main function
  * this program prints the size of various types on the computer
  * it is compiled and run on
- * Return : 0
+ * Return: 0
 */
 int main(void)
 {
