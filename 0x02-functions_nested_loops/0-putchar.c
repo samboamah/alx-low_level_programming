ad7560a0d0db848cbc30a5ff5988792ae7669d7d
Samuel Boamah <skboamah16@gmail.com>
2022-09-14 21:05:58 +0000

    0 done

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,6 +7,12 @@
  */
 int main(void)
 {
-	int _putchar("_putchar\n");
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
