3e6379c28b2ef0554a9ed6e91e7b1c00b6b5f5cb
Samuel Boamah <skboamah16@gmail.com>
2022-09-14 23:41:09 +0000

    2 done again

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,14 +7,14 @@
  */
 void print_alphabet_x10(void)
 {
+	int b;
 	char a;
-	int i;
 
-	for (i = 0; i < 10; i++)
+	for (b = 0; b < 10; b++)
 	{
 		for (a = 'a'; a <= 'z'; a++)
 		{
-			_putchar(i);
+			_putchar(a);
 		}
 	_putchar('\n');
 	}
