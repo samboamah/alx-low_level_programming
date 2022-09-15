3ebbe8b57047d0b0791fbd9d56763a962cc61819
Samuel Boamah <skboamah16@gmail.com>
2022-09-15 17:56:38 +0000

    9 done

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,7 +13,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 101; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
@@ -29,9 +29,9 @@ int main(void)
 		}
 		else
 		{
-			printf("%i ", i);
+			printf("%d ", i);
 		}
 	}
-	printf("\n");
+	printf("Buzz\n");
 	return (0);
 }
