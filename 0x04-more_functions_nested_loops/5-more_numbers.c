dddf2524fa3232b9fea2bbdce1988a180db0a895
Samuel Boamah <skboamah16@gmail.com>
2022-09-15 14:49:29 +0000

    5 done

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,9 +11,13 @@ void more_numbers(void)
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j = 14; j++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar(j);
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
 		}
 	_putchar('\n');
 	}
