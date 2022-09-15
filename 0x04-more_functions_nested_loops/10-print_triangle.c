b55ec39c1433737eed231716f9405c450902115f
Samuel Boamah <skboamah16@gmail.com>
2022-09-15 18:10:29 +0000

    10 done

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,3 +6,26 @@
  * @size: size of tirangle
  *
  * Return: no return
+ */
+void print_triangle(int size)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 1; b < (size - a); b++)
+		{
+			_putchar(32);
+		}
+		for (b--; b < size; b++)
+		{
+			_putchar('#');
+		}
+		if (a < (size - 1))
+		{
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
