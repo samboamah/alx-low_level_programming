79090568f35ddadbfede080d654cbb197c1d3c79
Samuel Boamah <skboamah16@gmail.com>
2022-09-20 03:23:02 +0000

    3 done

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, with a new line to stdout
+ *
+ * @str: pointer to string to print
+ *
+ * Return: no return
+ */
+void _puts(char *str)
+{
+	int new = 0;
+
+	while (str[new])
+	{
+		_putchar(str[new]);
+		new++;
+	}
+	_putchar('\n');
+
+
