18a483ed635923c22439bbcc1f49567b9e2c39a0
Samuel Boamah <skboamah16@gmail.com>
2022-09-20 04:13:09 +0000

    3 done again

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,18 +7,15 @@
  *
  * Return: no return
  */
+
 void _puts(char *str)
 {
-	int count = 0;
+	int i = 0;
 
-	while (count >= 0)
+	while (*(str + i) != '\0')
 	{
-		if (str[count] == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
-		_putchar(str[count]);
-		count++;
+		putchar(*(str + i));
+		i++;
 	}
+	putchar(10);
 }
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every character of a string
+ *
+ * @str: A pointer to an int that will be changed
+ *
+ * Return: no return
+ */
+void puts2(char *str)
+{
+	int s;
+
+	for (s = 0; str[s] != '\0'; s++)
+	{
+		if (s % 2 == 0)
+	}		_putchar (str[s]);
+
+	_putchar ('\n');
+}
