cbf99bd528c1200b3da5d8a354ad2e363fa002fc
Samuel Boamah <skboamah16@gmail.com>
2022-10-01 01:29:55 +0000

    done

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,9 @@
  * @argv: argument that stores the strings in an array of char*
  * Return: 0
  */
-int main(int argc, char* argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+
 	return (0);
 }
