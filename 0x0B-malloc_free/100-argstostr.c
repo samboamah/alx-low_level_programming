8d866377501d3604c7adf50b80a2279b27f9e654
Samuel Boamah <skboamah16@gmail.com>
2022-10-06 13:10:00 +0000

    malloc

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,31 +10,37 @@
  * Return: If ac == 0, av == NULL, or the function fails - NULL.
  * Otherwise - a pointer to the new string.
  */
+
 char *argstostr(int ac, char **av)
 {
 	char *str;
-	int arg, byte, i = 0, size = ac;
+	int arg, byte, index, size = ac;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	for (arg = 0; arg < ac; arg++)
+	{
 		for (byte = 0; av[arg][byte]; byte++)
 			size++;
+	}
 
 	str = malloc(sizeof(char) * size + 1);
 
 	if (str == NULL)
 		return (NULL);
 
+	index = 0;
+
 	for (arg = 0; arg < ac; arg++)
 	{
 		for (byte = 0; av[arg][byte]; byte++)
-			str[i++] = av[arg][byte];
+			str[index++] = av[arg][byte];
 
-		str[i++] = '\n';
+		str[index++] = '\n';
 	}
 
 	str[size] = '\0';
+
 	return (str);
 }
