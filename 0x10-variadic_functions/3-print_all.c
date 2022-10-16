4db4c9851f5c6ed7c8c66fd959896e3db56aef59
Samuel Boamah <skboamah16@gmail.com>
2022-10-16 01:00:53 +0000

    functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,26 +1,51 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - prints numbers.
- * @separator: string to be printed between numbers.
- * @n: number of integers passed to the function.
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function.
  *
  * Return: no return.
  */
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_all(const char * const format, ...)
 {
 	va_list valist;
-	unsigned int i;
+	unsigned int i = 0, j, c = 0;
+	char *str;
+	const char t_arg[] = "cifs";
 
-	va_start(valist, n);
-
-	for (i = 0; i < n; i++)
+	va_start(valist, format);
+	while (format && format[i])
 	{
-		printf("%d", va_arg(valist, int));
-		if (separator && i < n - 1)
-			printf("%s", separator);
+		j = 0;
+		while (t_arg[j])
+		{
+			if (format[i] == t_arg[j] && c)
+			{
+				printf(", ");
+				break;
+			} j++;
+		}
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(valist, int)), c = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(valist, int)), c = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(valist, double)), c = 1;
+			break;
+		case 's':
+			str = va_arg(valist, char *), c = 1;
+			if (!str)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", str);
+			break;
+		} i++;
 	}
-
-	printf("\n");
-	va_end(valist);
+	printf("\n"), va_end(valist);
 }
